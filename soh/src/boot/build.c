1860f06d82c0bdcc501b9591fe6851c20c54bf06
cplaster <nixhex@gmail.com>
2024-10-14 09:50:18 -0500

    update to new develop branch

---
+++ soh/src/boot/build.c
@@ -0,0 +1,14 @@
+#include <libultraship/libultra.h>
+
+const char gBuildVersion[] = "MacReady Golf (8.0.6)";
+const u16 gBuildVersionMajor = 8;
+const u16 gBuildVersionMinor = 0;
+const u16 gBuildVersionPatch = 6;
+
+const char gGitBranch[] = "develop";
+const char gGitCommitHash[] = "3daf7a2ee5b5b1c3646e9f46c6c6b56c823a71dc";
+const char gGitCommitTag[] = "";
+
+const char gBuildTeam[] = "github.com/harbourmasters";
+const char gBuildDate[] = __DATE__ " " __TIME__;
+const char gBuildMakeOption[] = "";
